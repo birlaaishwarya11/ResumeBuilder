574628a2afb66a79ceaa89c39cf9d6ec95594c37
Aishwarya Birla <aishwaryabirla@dhcp-vl2051-975.eduroam.cornell.edu>
2026-01-22 16:34:41 -0500

    Allow port configuration via environment variable

--- app.py
+++ app.py
@@ -411,4 +411,5 @@ def download_file(filename):
 
 if __name__ == '__main__':
     # Listen on all interfaces for Daytona access
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
